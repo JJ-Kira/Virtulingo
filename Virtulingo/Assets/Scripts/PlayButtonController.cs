f6d41c70482d0b3f682314808f3c5d0f1efe5fad
Klaudia O <62263707+Loxx-KO@users.noreply.github.com>
2024-06-11 00:34:36 +0200

    Visuals

---
+++ Virtulingo/Assets/Scripts/PlayButtonController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButtonController : MonoBehaviour
+{
+    public bool IsPressed = false;
+    private bool animating = false;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Blackboard blackboard;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!animating)
+        {
+            animating = true;
+            if (!IsPressed)
+            {
+                animator.Play("pushDown");
+                IsPressed = true;
+            }
+            else
+            {
+                animator.Play("resetButton");
+                IsPressed = false;
+            }
+        }
+    }
+
+    public void StartPistolVocab()
+    {
+        animating = false;
+        blackboard.ChangeContent(ActiveContent.PistolWhip);
+    }
+
+    public void StopPistolVocab()
+    {
+        animating = false;
+        blackboard.ChangeContent(ActiveContent.Menu);
+    }
+
+    public void StartMemoMingle()
+    {
+        animating = false;
+        blackboard.ChangeContent(ActiveContent.MemoMingle);
+    }
+
+    public void StopMemoMingle()
+    {
+        animating = false;
+        blackboard.ChangeContent(ActiveContent.Menu);
+    }
+}
