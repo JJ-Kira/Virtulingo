be4ff9e8f6943eb3bd8c2f3d1d15182862808f0e
Julia Szczuczko <julia@adcon.pl>
2024-06-10 15:06:35 +0200

    main menu setup

--- Virtulingo/Assets/Scripts/MainMenu.cs
+++ Virtulingo/Assets/Scripts/MainMenu.cs
@@ -1,14 +1,18 @@
 using DG.Tweening;
 using UnityEngine;
 using System;
+using TMPro;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour, IContent
 {
-    [SerializeField] private CanvasGroup cgMenu;
+    [SerializeField] private CanvasGroup cgMenu,htpButton;
+    [SerializeField] private RectTransform exitTransform, titleTransform;
+    [SerializeField] private RawImage instructions;
     
     private void Start()
     {
-        cgMenu.DOFade(1.0f, 0.75f);
+        cgMenu.DOFade(1.0f, 0.3f);
     }
     
     public void Disable(Action onComplete)
@@ -17,5 +21,21 @@ public class MainMenu : MonoBehaviour, IContent
             gameObject.SetActive(false);
             onComplete?.Invoke();
         };
+    }
+
+    public void ShowInstructions()
+    {
+        htpButton.DOFade(0f, 0.3f).onComplete = () =>
+            {
+                Destroy(htpButton.gameObject);
+                instructions.DOFade(1.0f, 0.5f);
+            };
+        exitTransform.DOAnchorPosY(240f, 0.5f);
+        titleTransform.DOAnchorPosY(-240f, 0.5f);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
